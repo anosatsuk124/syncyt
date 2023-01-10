884fedb4145c6fd64dbfc4bb0930ab053e9796a1
Satsuki Akiba <anosatsuk124@gmail.com>
2023-01-10 22:02:45 +0900

    Create mocks reusing from my old project.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,49 @@
+import { Server } from 'ws';
+import express from 'express';
+import http from 'http';
 import { config } from 'dotenv';
 
-const result = config({
-    path: '../../.env',
+const result = config();
+
+const PORT = Number(process.env.PORT) || 3000;
+
+const app = express();
+const server = http.createServer(app);
+
+const wss = new Server({ server: server });
+const clientsByChannel = new Map();
+
+wss.on('connection', (client, req) => {
+    const channel = new URL(
+        req.url as string,
+        `http://${req.headers.host}`
+    ).pathname
+        .split('/')
+        .slice(-1)[0];
+
+    if (!clientsByChannel.has(channel)) {
+        clientsByChannel.set(channel, new Set());
+    }
+
+    clientsByChannel.get(channel).add(client);
+
+    client.on('message', (data) => {
+        for (const receiver of clientsByChannel.get(channel) || []) {
+            if (receiver == client) continue;
+            try {
+                receiver.send(data);
+            } catch (e) {
+                console.warn(e);
+            }
+        }
+
+        console.log('%s', data);
+        client.send(data);
+    });
 });
 
-if (result.error) {
-    throw result.error;
-}
+app.use('/', express.static(__dirname + '/../frontend/dist'));
+
+server.listen(PORT, () => {
+    console.log(`listening on http://localhost:${PORT}`);
+});
